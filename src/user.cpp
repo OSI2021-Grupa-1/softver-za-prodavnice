7814337865c26080e46f2f5b5641b3103db895f5
unknown <elenaa.djurdjevic@gmail.com>
2021-12-31 18:02:46 +0100

    dodala klase item i user

--- src/user.cpp
+++ src/user.cpp
@@ -1,1 +1,18 @@
+#include "softver-za-prodavnice/user.hpp"
 
+
+User::User(std::string username, std::string password, Position position, std::size_t number_of_logins) 
+	: username(username), password(password), position(position), number_of_logins(number_of_logins) {}
+
+User::User(const User& other)
+	: username(other.username), password(other.password), position(other.position),
+	  number_of_logins(other.number_of_logins) {}
+
+
+std::string User::get_username() const { return username; }
+
+std::string User::get_password() const { return password; }
+
+Position User::get_position() const { return position; }
+
+std::size_t User::get_number_of_logins() const { return number_of_logins; }
