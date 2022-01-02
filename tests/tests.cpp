8de1485682679dc749f0471e16231226dce30871
Nikola Jelic <jelicnikola37@gmail.com>
2022-01-02 20:24:59 +0100

    Omoguceno testiranje pomocu GTest

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,13 +1,25 @@
 #include <cassert>
 #include <fstream>
 #include <iostream>
+#include <gtest/gtest.h>
+#include "softver-za-prodavnice/config.hpp"
+#include "softver-za-prodavnice/util.hpp"
 
-int main() {
-	if (auto file = std::ifstream("config.txt")) {
-		std::cout << "Opened\n";
-		return 0;
-	} else {
-		std::cout << "Filed\n";
-		return 1;
-	}
+TEST(trim_whitespace, whitespace) {
+	const std::string_view whitespace = "whitespace";
+
+	std::string_view str1 = whitespace;
+	EXPECT_EQ(whitespace, util::trim_whitespace(str1));
+
+	str1 = "  whitespace";
+	EXPECT_EQ(whitespace, util::trim_whitespace(str1));
+
+	str1 = "whitespace   ";
+	EXPECT_EQ(whitespace, util::trim_whitespace(str1));
+
+	str1 = "  whitespace      ";
+	EXPECT_EQ(whitespace, util::trim_whitespace(str1));
+
+	str1 = "whitespace";
+	EXPECT_NE(whitespace, util::trim_whitespace(str1));
 }
